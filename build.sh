ce27a4ccdac33e8b7e73b4c992bd7590c9246e41
tzmuda <tomasz.a.zmuda@gmail.com>
2017-07-26 19:50:02 +0200

    Build test

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ dotnet test Xabe.VideoConverter.Test/
 if [[ -z "${TRAVIS_TAG}" ]]; then 
 	exit 0
 else
-
+	cd Xabe.VideoConverter
+	dotnet clean -c Release
+	dotnet publish -c Release /property:Version=$TRAVIS_TAG -o VideoConverter
+	zip -r VideoConverter$TRAVIS_TAG.zip VideoConverter/*
+	/usr/bin/sshpass -p $SSH_PASSWORD scp VideoConverter$TRAVIS_TAG.zip $SSH_USER@$SSH_HOST:~/VideoConverter/VideoConverter$TRAVIS_TAG.zip
 fi
